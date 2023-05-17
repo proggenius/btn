e233393d4f4d5e89b9eab53454c4c4b360c92427
proggenius <133808488+proggenius@users.noreply.github.com>
2023-05-17 12:47:05 +0600

    finish

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelectorAll('.btn');
+
+for(let i = 0; i < btn.length; i++){
+    btn[i].addEventListener('click', function(e){
+        btn[i].classList.toggle('button-clicked');
+        btn[i].firstElementChild.classList.toggle
+        ('icon-cliked');
+
+    })
+}
